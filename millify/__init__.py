c7f9fb4a0e32d670ab9bd299076c6453bb977380
Gordon Wall <gordon.wall@vk.ai>
2021-06-17 13:36:22 -0400

    revised logic to determining millidx
    fixes 999 bug

--- millify/__init__.py
+++ millify/__init__.py
@@ -1,16 +1,9 @@
-import math
 import re
-from decimal import Decimal
 
 __author__ = "Alexander Zaitsev (azaitsev@gmail.com)"
 __copyright__ = "Copyright 2018, azaitsev@gmail.com"
 __license__ = "MIT"
 __version__ = "0.1.1"
-
-
-def remove_exponent(d):
-    """Remove exponent."""
-    return d.quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
 
 
 def millify(n, precision=0, drop_nulls=True, prefixes=[]):
@@ -20,12 +13,14 @@ def millify(n, precision=0, drop_nulls=True, prefixes=[]):
         millnames = ['']
         millnames.extend(prefixes)
     n = float(n)
-    millidx = max(0, min(len(millnames) - 1,
-                         int(math.floor(0 if n == 0 else math.log10(abs(n)) / 3))))
-    result = '{:.{precision}f}'.format(n / 10**(3 * millidx), precision=precision)
+    millidx = 0
+    while abs(n) >= 1000:
+        millidx += 1
+        n = round(n / 1000.0, precision)
+    result = '{}'.format(n)
     if drop_nulls:
-        result = remove_exponent(Decimal(result))
-    return '{0}{dx}'.format(result, dx=millnames[millidx])
+        result = result.rstrip('0').rstrip('.')
+    return '{}{dx}'.format(result, dx=millnames[millidx])
 
 
 def prettify(amount, separator=','):
